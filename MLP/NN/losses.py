7829f8e44ba807c34afde3e469129280a9f1c82b
Aditya Madhav Mantri <adityamantri1103@gmail.com>
2026-05-20 22:49:27 +0530

    Loss fuunctions defined

--- MLP/NN/losses.py
+++ MLP/NN/losses.py
@@ -22,3 +22,35 @@ Computes:
 
 This gradient starts the backward propagation process.
 ''' 
+
+import numpy as np
+
+class MSELoss:
+    def forward(self,predictions,targets):
+        self.predictions=predictions
+        self.targets=targets # this is caching ie... we are storing values for backward pass
+
+        loss=np.mean((targets-predictions)**2)
+        return loss
+    
+    def backward(self):
+        batch_size=self.targets.shape[0]
+        gradient = (2*(self.predictions-self.targets))/batch_size
+        return gradient
+
+class BCELoss:
+    def forward(self,predictions,targets):
+        predictions=np.clip(predictions,1e-7,1-1e-7)
+
+        self.predictions=predictions
+        self.targets=targets
+
+        loss = -np.mean((targets*np.log(predictions))+
+                        (1-targets)*np.log(1-predictions))
+        return loss
+    
+    def backward(self):
+        batch_size=self.targets.shape[0]
+
+        gradient = (self.predictions-self.targets)/(self.predictions*(1-self.predictions))/batch_size
+        return gradient
